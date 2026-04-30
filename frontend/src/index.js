43df785cdbaccbd66ec5e0a2825b60ef02999e84
Anubhav Srivastava <2k22cscys2211541@gmail.com>
2026-05-01 05:00:28 +0530

    fix css build

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,14 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-// ✅ IMPORTANT: import CSS
-import './index.css';
-import './App.css';
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<React.StrictMode><App /></React.StrictMode>);
