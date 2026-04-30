9e5503a6fd3b239525128e56cbf914dd0b4adc15
Anubhav Srivastava <2k22cscys2211541@gmail.com>
2026-05-01 01:46:51 +0530

    fix: use DATABASE_URL

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(process.env.MYSQL_URL, {
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'mysql',
   logging: false,
 });
